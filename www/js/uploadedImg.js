a2196e6cab81b43551eee13a2d054302950c897a
LSdata <mail.linnea.s@gmail.com>
2016-02-28 07:37:45 +0100

    update
    update

---
+++ www/js/uploadedImg.js
@@ -0,0 +1,12 @@
+
+
+function showTwitterBtn(){
+  var postTwBtn = document.getElementById('postOnTwitterBtn');
+  postTwBtn.style.visibility = 'visible';
+}
+
+function saveImg(){
+    var img = document.getElementById('saveImg');
+    img.style.visibility = 'visible';
+
+}
